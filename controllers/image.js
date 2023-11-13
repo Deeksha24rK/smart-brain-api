431fe49045b56a217f1335f5aedd00cb17be50ef
Karthik-kotian <deeksha24rk@gmail.com>
2023-11-13 16:01:49 +0530

    moved clarifai API to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,41 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+const stub = ClarifaiStub.grpc();
+
+const handleApiCall = (req, res) => {
+    const { imageURL } = req.body
+    const PAT = 'd1b4b3da011e4c58ae317e8e4d31a9f4';
+    const USER_ID = '59zf7da7czjz';
+    const APP_ID = 'my-first-application-0290u';
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageURL;
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", "Key " + PAT);
+
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            model_id: MODEL_ID,
+            inputs: [
+                { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                throw new Error(err);
+            }
+
+            if (response.status.code !== 10000) {
+                throw new Error("Post model outputs failed, status: " + response.status.description);
+            }
+            res.json(response)
+        }
+    );
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body
     db('users')
@@ -11,5 +49,5 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage, handleApiCall
 }
